8e33057b59c779126c46c17a5dc98ff755a427c0
Aziz Shahmardanli <aziz.shahmardan@gmail.com>
2023-07-09 00:37:30 +0400

    add home and menu pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import "./style.css";
 import Icon from "./imgs/bologna.png";
+import Photo from "./imgs/luigis-2.webp";
 const content = document.querySelector("#content");
-
-console.log("hello world");
 
 (function addHeader() {
    const header = document.createElement("header");
@@ -26,3 +25,72 @@ console.log("hello world");
    header.appendChild(nav);
    content.appendChild(header);
 })();
+
+function addHome() {
+   const main = document.createElement("main");
+   const h2 = document.createElement("h2");
+   h2.appendChild(
+      document.createTextNode("Traditional dishes of the Emilian cuisine")
+   );
+   main.appendChild(h2);
+   const photo = new Image();
+   photo.src = Photo;
+   main.appendChild(photo);
+   const para = document.createElement("p");
+   para.appendChild(
+      document.createTextNode(
+         "A timeless cuisine serving dishes of the Emilian tradition: from the" +
+            " most popular dishes, like tagliatelle Bolognese and tortellini in" +
+            " broth, to the rare and much-appreciated boiled meat"
+      )
+   );
+   main.appendChild(para);
+   content.appendChild(main);
+}
+addHome();
+
+let menuArr = [
+   { name: "Tagliatelle Bolognese", price: 21 },
+   { name: "Tagliolini with Culatello di Zibello in White Sauce", price: 21 },
+   { name: "Tortellini Bolognese", price: 24 },
+   { name: "Spaghettini with Bottarga (Mullet Roe)", price: 24 },
+   { name: "Green Lasagna Bolognese", price: 22 },
+   { name: "Homemade Tortelloni with Ricotta Cheese and Spinach", price: 22 },
+   { name: "Bolognese Fried Risotto", price: 22 },
+   { name: "Gragnano Spaghetti with Lobster", price: 38 },
+   { name: "Fresh Tomato Soup", price: 18 }
+];
+
+function addMenu() {
+   const sect = document.createElement("section");
+   const h2 = document.createElement("h2");
+   h2.appendChild(document.createTextNode("Menu"));
+   sect.appendChild(h2);
+   const table = document.createElement("table");
+   const tHead = document.createElement("thead");
+   const thr = document.createElement("tr");
+   ["Meal", "Price"].forEach((word) => {
+      let th = document.createElement("th");
+      th.appendChild(document.createTextNode(word));
+      thr.appendChild(th);
+   });
+   const tBody = document.createElement("tbody");
+
+   menuArr.forEach((item) => {
+      const tr = document.createElement("tr");
+      const dataName = document.createElement("td");
+      dataName.appendChild(document.createTextNode(item.name));
+      tr.appendChild(dataName);
+      const dataPrice = document.createElement("td");
+      dataPrice.classList.add('price')
+      dataPrice.appendChild(document.createTextNode(item.price));
+      tr.appendChild(dataPrice);
+      tBody.appendChild(tr);
+   });
+   tHead.appendChild(thr);
+   table.appendChild(tHead);
+   table.appendChild(tBody);
+   sect.appendChild(table);
+   content.appendChild(sect);
+}
+addMenu();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-   mode: "production",
+   mode: "development",
    entry: "./src/index.js",
    output: {
       filename: "main.js",
@@ -15,7 +15,7 @@ module.exports = {
             use: ["style-loader", "css-loader"]
          },
          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
             type: "asset/resource"
          },
          {
